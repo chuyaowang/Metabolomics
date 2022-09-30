d4229bde0bdd8bfbcbd003a3afac81b6b47b119d
root <root@LAPTOP-N81N97MG.localdomain>
2022-09-30 17:59:53 +0800

    Added more to the CAMERA script; Added script to get abundance; Added script to plot eic and spectrum

--- CAMERA.R
+++ CAMERA.R
@@ -18,66 +18,15 @@ data <- readMSData(file, # filenames to load
                    mode = "onDisk" # Only load metadata into memory and leave the m/z ratio vs intensity peaks on disk
 ) 
 
-pos <- data %>%
-  filterPolarity(1) %>%
-  filterMsLevel(1)
-
-neg <- data %>%
-  filterPolarity(0) %>%
-  filterMsLevel(1)
-
-dataA <- list(pos,neg)
-rm(data)
 # Data summary -----
 featureData <- fData(data)
 
-polarity(data) %>% table
+polarity(data %>% filterPolarity(1)) %>% table
 msLevel(data) %>% table
 
 # Data processing -----
-chr <- 
+chr <- chromatogram(data, aggregationFun = "max")
 
+dim(chr)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-xs <- xcmsSet(
-  files=file, 
-  method="centWaveWithPredictedIsotopeROIs",
-  ppm=10,
-  mslevel = 1,
-  snthresh = 5,
-  integrate = 1,
-  peakwidth = c(2,30),
-  prefilter = c(1,1e4),
-  mzCenterFun = "wMeanApex3",
-  mzdiff = -0.001
-)
-
-an <- xsAnnotate(xs, polarity="negative") # constructor; extracts peak table
-an <- groupFWHM(an, perfwhm = 1) # group peaks by retention time
-an <- findIsotopesWithValidation(object = an, ppm = 10,
-                                 mzabs = 0.01, intval="intb",
-                                 maxcharge = 3) # annotate isotopic peaks
-## extract annotated peak table
-peakTable <- getPeaklist(an) # extract peak list
+plot(chr %>% `[[`(1,1))
